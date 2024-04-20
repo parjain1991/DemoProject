9a306c654276f944ebcf3828005d9f41470213a7
paruljain1966 <parul.jain@publicissapient.com>
2024-04-20 11:36:21 +0530

    added car app

---
+++ CarApp/CarApp/CarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAppApp.swift
+//  CarApp
+//
+//  Created by Parul Jain on 08/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
